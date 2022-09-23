776967b216ed4696a70c6cfda25b798c645bfcb3
KoFE <petrovykh03@gmail.com>
2022-09-23 21:08:54 +0300

    Third

--- gulpfile.js
+++ gulpfile.js
@@ -71,9 +71,8 @@ gulp.task('icons', function () {
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
-        .pipe(imagemin())
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images', 'php'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'images', 'html', 'php'));
