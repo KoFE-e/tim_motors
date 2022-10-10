25c50427c522e5c192a62fbedd53c0129ed0cfb7
KoFE <petrovykh03@gmail.com>
2022-10-10 23:08:14 +0300

    popup

--- dist/js/script_catalog.js
+++ dist/js/script_catalog.js
@@ -323,6 +323,60 @@ dropdowns.forEach(dropdown => {
     });
 });
 
+const more_btns = document.querySelectorAll('.catalog__card-more');
+const card_info = document.querySelectorAll('.catalog__card__info');
+const close_btns = document.querySelectorAll('.catalog__card__info-close');
+const card_img = document.querySelectorAll('.catalog__card-img');
+
+more_btns.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        fadeIn(card_info[index], 600);
+        fadeIn(overlay, 600);
+    });
+});
+
+card_img.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        fadeIn(card_info[index], 600);
+        fadeIn(overlay, 600);
+    });
+});
+
+close_btns.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        fadeOut(card_info[index], 600);
+        fadeOut(overlay, 600);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    card_info.forEach(item => {
+        if (getComputedStyle(item).display != 'none') {
+            fadeOut(item, 600);
+            fadeOut(overlay, 600);
+        };
+    });
+});
+
+$(document).ready(function() {
+	$('.catalog__card__info-images').magnificPopup({
+		delegate: 'img',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
 
 
 
