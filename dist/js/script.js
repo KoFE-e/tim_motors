ddc71892a202fd8d192aed7ef54236d203946e4f
KoFE <petrovykh03@gmail.com>
2022-10-19 21:26:33 +0300

    fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -148,3 +148,37 @@ else {
         window.location.href = '/catalog_en.html' + '?' + searchParams;
     });
 }
+
+//youtube_mobile_video_autoplay
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+        
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('team__video__iframe', {
+    videoId: 'VbTipiDoETM',
+    playerVars: { 'autoplay': 1, 'playsinline': 1,
+                    'version': 3, 'controls': 0,
+                    'showinfo': 0, 'mute': 1,
+                    'loop': 1, 'playlist': 'VbTipiDoETM',
+                    'disablekb': 1, 'modestbranding': 1,
+                    'rel': 0
+                },
+    events: {
+    'onReady': onPlayerReady
+    }
+});
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+event.target.mute();
+event.target.playVideo();
+}
